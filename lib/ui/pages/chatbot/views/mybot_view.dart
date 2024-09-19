9c38615a0f8d2e66a5033578192b2273bd701e97
Dereck <dexcka3@gmail.com>
2024-09-19 18:20:37 -0500

    Vistas de home y perfil

--- lib/ui/pages/chatbot/views/mybot_view.dart
+++ lib/ui/pages/chatbot/views/mybot_view.dart
@@ -3,6 +3,8 @@ import 'package:chat_gpt/ui/providers/providers.dart';
 import 'package:dash_chat_2/dash_chat_2.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'home_view.dart'; // Importa la vista de Home
+import 'profile_view.dart'; // Importa la vista de Perfil
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -19,6 +21,7 @@ class ChatBot extends ConsumerStatefulWidget {
 
 class _ChatBotState extends ConsumerState<ChatBot> {
 
+  int _selectedIndex = 0;  // Variable para almacenar el índice de la vista seleccionada
   final FlutterTts _flutterTts = FlutterTts();
   List<Map> _voices = [];
   Map? _currentVoice;
@@ -30,6 +33,20 @@ class _ChatBotState extends ConsumerState<ChatBot> {
   List<ChatUser> typing = [];
   late final ChatbotController controller; //late, lo que se dice es que espere a que se le asignen datos
 
+  // Lista de las vistas a mostrar en el cuerpo del Scaffold
+  static List<Widget> _pages = <Widget>[
+    ChatBot(),  // Aquí va tu vista de ChatBot
+    HomeView(),     // Vista de Home
+    ProfileView(),  // Vista de Perfil
+  ];
+
+// Función para manejar el cambio de pestañas en el BottomNavigationBar
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+  
   @override
   void initState() {
     super.initState();
@@ -129,28 +146,68 @@ Widget build(BuildContext context) {
       centerTitle: true,
     ),
     body: SafeArea(
-      child: SizedBox(
-        height: size.height,
-        width: size.width,
-        child: DashChat(
-          inputOptions: const InputOptions(
-            sendOnEnter: true,
-            cursorStyle: CursorStyle(color: Colors.black),
+      child: Column(
+        children: [
+          Expanded(
+            child: SizedBox(
+              height: size.height * 0.9, // Ocupa el 90% de la altura
+              width: size.width,
+              child: _selectedIndex == 0 
+                ? DashChat(
+                    inputOptions: const InputOptions(
+                      sendOnEnter: true,
+                      cursorStyle: CursorStyle(color: Colors.black),
+                    ),
+                    messageOptions: MessageOptions(
+                      showTime: true,
+                      textColor: Colors.black,
+                      containerColor: const Color.fromARGB(204, 227, 207, 118),
+                      currentUserContainerColor: const Color.fromARGB(255, 181, 115, 40),
+                      avatarBuilder: yourAvatarBuilder,
+                    ),
+                    typingUsers: typing,
+                    currentUser: controller.usuario,
+                    onSend: getDataMessage,
+                    messages: stateController.mensajeEnv,
+                  )
+                : _pages[_selectedIndex], // Muestra la vista correspondiente si no es el chat
+            ),
           ),
-          messageOptions: MessageOptions(
-            showTime: true,
-            textColor: Colors.black,
-            containerColor: const Color.fromARGB(204, 227, 207, 118),
-            currentUserContainerColor: const Color.fromARGB(255, 181, 115, 40),
-            avatarBuilder: yourAvatarBuilder,
+          Container(
+            height: size.height * 0.1, // Ocupa el 10% de la altura
+            width: size.width,
+            color: Colors.grey, // Cambia el color según lo que necesites
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround, // Distribuye los botones de manera uniforme
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.chat),
+                  color: Colors.white,
+                  onPressed: () {
+                    _onItemTapped(0); // Cambia a la vista de chat
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.home),
+                  color: Colors.white,
+                  onPressed: () {
+                    _onItemTapped(1); // Cambia a la vista de home
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.person),
+                  color: Colors.white,
+                  onPressed: () {
+                    _onItemTapped(2); // Cambia a la vista de perfil
+                  },
+                ),
+              ],
+            ),
           ),
-          typingUsers: typing,
-          currentUser: controller.usuario,
-          onSend: getDataMessage,
-          messages: stateController.mensajeEnv,
-        ),
+        ],
       ),
     ),
+    
     floatingActionButton: Column(
       mainAxisAlignment: MainAxisAlignment.end,
       crossAxisAlignment: CrossAxisAlignment.end,
